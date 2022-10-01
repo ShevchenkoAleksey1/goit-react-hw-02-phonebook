1f410ea99037c01dd5a5991454fd7fe4b55a1e23
VitaPetrushyna <alexres87@gmail.com>
2022-10-01 21:06:52 +0300

    Contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Componet } from 'react';
+export class App extends Componet {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+}
